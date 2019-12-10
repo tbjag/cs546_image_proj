2b9c37a19f4ce67e666adf1c10808431de28c4c5
tbjag <48359620+tbjag@users.noreply.github.com>
2019-12-10 17:52:48 -0500

    recommented profile

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const constructorMethod = app => {
     //first route for when user first accesses site
     app.use("/", homeRoute);
     //auth route: if not auth, redirect to home, if auth then display correct profile and info
-    app.use("/profile", usersRoute);
+    //app.use("/profile", usersRoute);
     //redirects to editing page to display proper filters
     //app.use("/filter", filterRoute);
 
