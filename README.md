89b53a16427d35fd1d0abb543f476278584047ec
Theodore J <tbjagodits@gmail.com>
2019-12-10 10:47:02 -0500

    started routes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # cs546_image_proj
 
-hello and welcome to our cs 546 image project. 
+## hello and welcome to our cs 546 image project. 
 
 to do:
 
---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const usersRoute = require("./users");
+const dataRoute = require("./data");
+const filterRoute = require("./filters");
+const homeRoute = require("./home")
+
+const constructorMethod = app => {
+    //proj routes
+    //first route for when user first accesses site
+    app.use("/", homeRoute);
+    //auth route: if not auth, redirect to home, if auth then display correct profile and info
+    app.use("/users", usersRoute);
+    //stores images, handles image requests from authorized users
+    app.use("/data", dataRoute);
+    //redirects to editing page to display proper filters
+    app.use("/filter", filterRoute);
+
+    app.use("*", (req, res) => {
+        res.redirect("/");
+    });
+};
+
+module.exports = constructorMethod;
