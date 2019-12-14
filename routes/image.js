ca85a5f8170ed82ec9a5078d72e04f9feccd6331
Alex Kubecka <akubecka@stevens.edu>
2019-12-14 00:38:51 -0500

    Update image.js

--- routes/image.js
+++ routes/image.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const data = require("../data");
 const usersData = data.users;
+const multer = require('multer');
 
 // SET STORAGE
 var storage = multer.diskStorage({
