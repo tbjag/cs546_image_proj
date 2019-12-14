8b0c407c39ad4dd38ce2a65cd96f696a9c6a414c
Sam Kraus <skraus@stevens.edu>
2019-12-14 00:40:08 -0500

    added the bad layouts for each filter type

--- routes/filters.js
+++ routes/filters.js
@@ -13,6 +13,258 @@ const router = express.Router();
 const data = require("../data");
 const usersData = data.users;
 
+router.post("/kmeans", async function (req,res){
+    console.log("kmeansFILTER");
+    if(req.session.userId){
+    try{
+      if(req.session.userId){
+        const inName = req.body.inputName
+        const outName = req.body.outputName
+        const k = req.body.k
+        console.log(inName)
+        console.log(outName)
+        console.log(k)
+        const profile = await usersData.get(req.session.userId);
+        //const rend = "layouts/" + filter
+        // actually run the filter here
+        res.render("layouts/home", {logged:true, username: profile.firstName});
+        return;
+      }else{
+        res.render("layouts/filter", {title: "PSLite Filters", logged:false});
+        return;
+      }
+    }catch(e){
+      console.log(e);
+      res.sendStatus(500);
+    }
+  }else{
+    res.render("layouts/filter", {logged:false});
+  }
+  });
+
+router.post("/greyscale", async function (req,res){
+    console.log("greryscaleFILTER");
+    if(req.session.userId){
+    try{
+      if(req.session.userId){
+        const inName = req.body.inputName
+        const outName = req.body.outputName
+        console.log(inName)
+        console.log(outName)
+        const profile = await usersData.get(req.session.userId);
+        //const rend = "layouts/" + filter
+        // actually run the filter here
+        res.render("layouts/home", {logged:true, username: profile.firstName});
+        return;
+      }else{
+        res.render("layouts/filter", {title: "PSLite Filters", logged:false});
+        return;
+      }
+    }catch(e){
+      console.log(e);
+      res.sendStatus(500);
+    }
+  }else{
+    res.render("layouts/filter", {logged:false});
+  }
+  });
+
+router.post("/invert", async function (req,res){
+    console.log("invertFILTER");
+    if(req.session.userId){
+    try{
+      if(req.session.userId){
+        const inName = req.body.inputName
+        const outName = req.body.outputName
+        console.log(inName)
+        console.log(outName)
+        const profile = await usersData.get(req.session.userId);
+        //const rend = "layouts/" + filter
+        // actually run the filter here
+        res.render("layouts/home", {logged:true, username: profile.firstName});
+        return;
+      }else{
+        res.render("layouts/filter", {title: "PSLite Filters", logged:false});
+        return;
+      }
+    }catch(e){
+      console.log(e);
+      res.sendStatus(500);
+    }
+  }else{
+    res.render("layouts/filter", {logged:false});
+  }
+  });
+
+router.post("/sepia", async function (req,res){
+    console.log("sepiaFILTER");
+    if(req.session.userId){
+    try{
+      if(req.session.userId){
+        const inName = req.body.inputName
+        const outName = req.body.outputName
+        const k = req.body.k
+        console.log(inName)
+        console.log(outName)
+        const profile = await usersData.get(req.session.userId);
+        //const rend = "layouts/" + filter
+        // actually run the filter here
+        res.render("layouts/home", {logged:true, username: profile.firstName});
+        return;
+      }else{
+        res.render("layouts/filter", {title: "PSLite Filters", logged:false});
+        return;
+      }
+    }catch(e){
+      console.log(e);
+      res.sendStatus(500);
+    }
+  }else{
+    res.render("layouts/filter", {logged:false});
+  }
+  });
+
+router.post("/remBlue", async function (req,res){
+    console.log("remBlueFILTER");
+    if(req.session.userId){
+    try{
+      if(req.session.userId){
+        const inName = req.body.inputName
+        const outName = req.body.outputName
+        console.log(inName)
+        console.log(outName)
+        const profile = await usersData.get(req.session.userId);
+        //const rend = "layouts/" + filter
+        // actually run the filter here
+        res.render("layouts/home", {logged:true, username: profile.firstName});
+        return;
+      }else{
+        res.render("layouts/filter", {title: "PSLite Filters", logged:false});
+        return;
+      }
+    }catch(e){
+      console.log(e);
+      res.sendStatus(500);
+    }
+  }else{
+    res.render("layouts/filter", {logged:false});
+  }
+  });
+
+router.post("/remRed", async function (req,res){
+    console.log("remRedFILTER");
+    if(req.session.userId){
+    try{
+      if(req.session.userId){
+        const inName = req.body.inputName
+        const outName = req.body.outputName
+        console.log(inName)
+        console.log(outName)
+        const profile = await usersData.get(req.session.userId);
+        //const rend = "layouts/" + filter
+        // actually run the filter here
+        res.render("layouts/home", {logged:true, username: profile.firstName});
+        return;
+      }else{
+        res.render("layouts/filter", {title: "PSLite Filters", logged:false});
+        return;
+      }
+    }catch(e){
+      console.log(e);
+      res.sendStatus(500);
+    }
+  }else{
+    res.render("layouts/filter", {logged:false});
+  }
+  });
+
+router.post("/remGreen", async function (req,res){
+    console.log("remGreenFILTER");
+    if(req.session.userId){
+    try{
+      if(req.session.userId){
+        const inName = req.body.inputName
+        const outName = req.body.outputName
+        console.log(inName)
+        console.log(outName)
+        const profile = await usersData.get(req.session.userId);
+        //const rend = "layouts/" + filter
+        // actually run the filter here
+        res.render("layouts/home", {logged:true, username: profile.firstName});
+        return;
+      }else{
+        res.render("layouts/filter", {title: "PSLite Filters", logged:false});
+        return;
+      }
+    }catch(e){
+      console.log(e);
+      res.sendStatus(500);
+    }
+  }else{
+    res.render("layouts/filter", {logged:false});
+  }
+  });
+
+router.post("/blur", async function (req,res){
+    console.log("blurFILTER");
+    if(req.session.userId){
+    try{
+      if(req.session.userId){
+        const inName = req.body.inputName
+        const outName = req.body.outputName
+        const k = req.body.k
+        console.log(inName)
+        console.log(outName)
+        console.log(k)
+        const profile = await usersData.get(req.session.userId);
+        //const rend = "layouts/" + filter
+        // actually run the filter here
+        res.render("layouts/home", {logged:true, username: profile.firstName});
+        return;
+      }else{
+        res.render("layouts/filter", {title: "PSLite Filters", logged:false});
+        return;
+      }
+    }catch(e){
+      console.log(e);
+      res.sendStatus(500);
+    }
+  }else{
+    res.render("layouts/filter", {logged:false});
+  }
+  });
+
+router.post("/posterize", async function (req,res){
+    console.log("posterizeFILTER");
+    if(req.session.userId){
+    try{
+      if(req.session.userId){
+        const inName = req.body.inputName
+        const outName = req.body.outputName
+        const k = req.body.k
+        console.log(inName)
+        console.log(outName)
+        console.log(k)
+        const profile = await usersData.get(req.session.userId);
+        //const rend = "layouts/" + filter
+        // actually run the filter here
+        res.render("layouts/home", {logged:true, username: profile.firstName});
+        return;
+      }else{
+        res.render("layouts/filter", {title: "PSLite Filters", logged:false});
+        return;
+      }
+    }catch(e){
+      console.log(e);
+      res.sendStatus(500);
+    }
+  }else{
+    res.render("layouts/filter", {logged:false});
+  }
+  });
+
+
+
 router.post("/filters", async function (req,res){
     console.log("specificFILTER");
     if(req.session.userId){
