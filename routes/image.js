ed80e777148bf9df68642b36a471292ed8d7e7ad
tbjag <48359620+tbjag@users.noreply.github.com>
2019-12-14 01:06:28 -0500

    changed routes

--- routes/image.js
+++ routes/image.js
@@ -9,7 +9,7 @@ const multer = require('multer');
 // SET STORAGE
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../public/images');
+    cb(null, './public/images');
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now());
