d0ae57104abde557b5cbf83917fb1877d4f0f8ca
Sam Kraus <skraus@stevens.edu>
2019-12-14 00:07:35 -0500

    started filter stuff

--- routes/filters.js
+++ routes/filters.js
@@ -13,6 +13,32 @@ const router = express.Router();
 const data = require("../data");
 const usersData = data.users;
 
+router.post("/filters", async function (req,res){
+    console.log("specificFILTER");
+    if(req.session.userId){
+    try{
+      if(req.session.userId){
+        const filter = req.body.filt
+        console.log(filter)
+        const profile = await usersData.get(req.session.userId);
+        const rend = "layouts/" + filter
+        res.render(rend, {logged:true, username: profile.firstName});
+        return;
+      }else{
+        res.render("layouts/filter", {title: "PSLite Filters", logged:false});
+        return;
+      }
+    }catch(e){
+      console.log(e);
+      res.sendStatus(500);
+    }
+  }else{
+    res.render("layouts/filter", {logged:false});
+  }
+  });
+
+
+
 router.get("/", async function (req,res){
     console.log("FILTER");
     if(req.session.userId){
